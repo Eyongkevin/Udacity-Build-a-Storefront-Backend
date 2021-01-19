f32f819eafec820eb80fef133223429b70d6fc15
Eyong Kevin <tonyparkerkenz@gmail.com>
2021-01-19 22:38:55 +0100

    Feats: Add logger
    Install morgan and set middleware to console log all requests.

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,14 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 
 import { routes } from './api/routes'
+import { morganMiddleware } from './api/middlewares/logger'
 
 
 export const app: express.Application = express()
 
 app.use(cors())
 app.use(bodyParser.json())
+
+app.use(morganMiddleware)
 
 routes(app);
