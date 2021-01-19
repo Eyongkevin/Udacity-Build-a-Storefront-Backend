90ed992e4094758b06a7d41eddf93debbc0de12f
Eyong Kevin <tonyparkerkenz@gmail.com>
2021-01-19 19:33:45 +0100

    Feats: User model and handler

---
+++ src/models/User.ts
@@ -0,0 +1,48 @@
+import { pool } from './Pool';
+import { Response, Request } from 'express'
+
+// define table
+const table: String = 'users';
+
+// set error message
+pool.on('error', (err, client) => `Error, ${err},  occured on ${client}`);
+
+// select all users
+const getUsers = async (req: Request, res: Response) => {
+    const allUsers = await pool.query(
+        `SELECT * FROM ${table};`
+    );
+    res.status(200).json(allUsers.rows);
+
+};
+
+// select user by id
+const getUserById = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const userById = await pool.query(
+        `SELECT * FROM ${table} WHERE id = $1`, [id]
+    );
+    res.status(200).json(userById.rows[0]);
+
+};
+
+// create a user
+const createUser = async (req: Request, res: Response) => {
+    const { firstName, lastName, password } = req.body;
+    const addUser = await pool.query(
+        `INSERT INTO ${table} (firstName, lastName, password) VALUES($1, $2, $3) RETURNING *`,
+        [firstName, lastName, password ]
+    );
+    res.status(200).json(addUser.rows[0]);
+
+};
+
+// update
+// delete
+// where
+
+export {
+    getUsers,
+    getUserById,
+    createUser
+}
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 
 import * as Product from './models/Product'
+import * as User from './models/User'
 
 const app: express.Application = express()
 const address: string = "0.0.0.0:3000"
@@ -14,9 +15,14 @@ app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
 })
 // Product route
-app.get('/product', Product.getProducts)
-app.get('/product:id', Product.getProductById)
-app.post('/product', Product.createProduct)
+app.get('/products', Product.getProducts)
+app.get('/products/:id', Product.getProductById)
+app.post('/products', Product.createProduct)
+
+// User route
+app.get('/users', User.getUsers)
+app.get('/users/:id', User.getUserById)
+app.post('/users', User.createUser)
 
 app.listen(3000, function () {
     console.log(`starting app on: ${address}`)
