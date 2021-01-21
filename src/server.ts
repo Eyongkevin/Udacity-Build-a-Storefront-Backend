195c3838e7e6f49e4874e75e197ac08f109339ea
Eyong Kevin <tonyparkerkenz@gmail.com>
2021-01-21 20:39:24 +0100

    Chore, Test: Add test for one endpoint
    Configure Jasmine and babel, then run test for one endpoint to get started.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import { app } from './app';
 
 const address: string = '0.0.0.0:3000';
 
-app.listen(3000, () => {
+export const server = app.listen(3000, () => {
   console.log(`starting app on: ${address}`);
 });
