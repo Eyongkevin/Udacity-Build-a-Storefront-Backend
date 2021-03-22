3eb2deace2c3f6b2ccbf1c64241005c22186e7fe
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2021-03-22 17:07:27 +0100

    Fixs; Add types to utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import jsonwebtoken from 'jsonwebtoken';
 
-export const generateToken = (id: number) => {
+export const generateToken: Function = (id: number): string => {
   return jsonwebtoken.sign(id.toString(), process.env.JWT_SECRET as string);
 };
