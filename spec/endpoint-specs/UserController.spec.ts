a1a6881e9555fd534bce829591bb765fa6e8ac9a
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2021-03-23 19:58:37 +0100

    Tests; Add tests for all endpoints to User controller
    All endpoints should have a test.

--- spec/endpoint-specs/UserController.spec.ts
+++ spec/endpoint-specs/UserController.spec.ts
@@ -19,6 +19,29 @@ describe('Test endpoint responses', () => {
         }
       ])
     );
+    spyOn(User.prototype, 'getUserById').and.returnValue(
+      Promise.resolve({
+        id: 1,
+        firstname: 'eyong',
+        lastname: 'kevin',
+        password: 'thisisenow'
+      })
+    );
+    spyOn(User.prototype, 'createUser').and.returnValue(
+      Promise.resolve({
+        auth: true,
+        token:
+          'eyJhbGciOiJIqzI1NiIsInRfcCI6IkpXVCJ9.330.J8BgsyqA3Y6F71NXbfuYyfRVuvRa_qb08RStxrCVhlQ'
+      })
+    );
+    spyOn(User.prototype, 'deleteUser').and.returnValue(
+      Promise.resolve({
+        id: 1,
+        firstname: 'eyong',
+        lastname: 'kevin',
+        password: 'thisisenow'
+      })
+    );
   });
 
   it('gets all users api endpoint', async (done) => {
@@ -35,6 +58,44 @@ describe('Test endpoint responses', () => {
         password: 'thisisenow'
       }
     ]);
+    done();
+  });
+  it('gets user by id api endpoint', async (done) => {
+    const res = await request
+      .get('/users/1')
+      .set('Authorization', 'Bearer ' + token);
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: 1,
+      firstname: 'eyong',
+      lastname: 'kevin',
+      password: 'thisisenow'
+    });
+    done();
+  });
+  it('create user api endpoint', async (done) => {
+    const res = await request
+      .post('/users')
+      .set('Authorization', 'Bearer ' + token);
+
+    expect(res.status).toBe(200);
+    expect(res.body.auth).toEqual(true);
+    expect(res.body.token).toBeDefined();
+    done();
+  });
+  it('delets a user api endpoint', async (done) => {
+    const res = await request
+      .delete('/users/1')
+      .set('Authorization', 'Bearer ' + token);
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: 1,
+      firstname: 'eyong',
+      lastname: 'kevin',
+      password: 'thisisenow'
+    });
     done();
   });
 });
