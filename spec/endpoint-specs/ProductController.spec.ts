79c9f0993ca9a7117b5c14a55b99b2e2a28977a0
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2021-03-23 20:17:12 +0100

    Tests; Create tests for all product endpoints

---
+++ spec/endpoint-specs/ProductController.spec.ts
@@ -0,0 +1,132 @@
+/* eslint-disable no-undef */
+import supertest from 'supertest';
+import { Product } from '../../src/api/models/Product';
+import { server } from '../../src/server';
+
+const request = supertest(server);
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.J8BgsyqA3Y6F71NXbfuYIfRVuvRa_qb08RStxrCVhlQ';
+
+describe('Test Product endpoint responses', () => {
+  beforeAll(() => {
+    spyOn(Product.prototype, 'getProducts').and.returnValue(
+      Promise.resolve([
+        {
+          id: 1,
+          name: 'iphone',
+          price: '3000',
+          category: 'phone'
+        }
+      ])
+    );
+    spyOn(Product.prototype, 'getProductById').and.returnValue(
+      Promise.resolve({
+        id: 1,
+        name: 'iphone',
+        price: '3000',
+        category: 'phone'
+      })
+    );
+    spyOn(Product.prototype, 'getProductByCat').and.returnValue(
+      Promise.resolve([
+        {
+          id: 1,
+          name: 'iphone',
+          price: '3000',
+          category: 'phone'
+        }
+      ])
+    );
+    spyOn(Product.prototype, 'createProduct').and.returnValue(
+      Promise.resolve({
+        id: 1,
+        name: 'iphone',
+        price: '3000',
+        category: 'phone'
+      })
+    );
+    spyOn(Product.prototype, 'deleteProduct').and.returnValue(
+      Promise.resolve({
+        id: 1,
+        name: 'iphone',
+        price: '3000',
+        category: 'phone'
+      })
+    );
+  });
+
+  it('gets all products api endpoint', async (done) => {
+    const res = await request
+      .get('/products')
+      .set('Authorization', 'Bearer ' + token);
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual([
+      {
+        id: 1,
+        name: 'iphone',
+        price: '3000',
+        category: 'phone'
+      }
+    ]);
+    done();
+  });
+  it('gets product by id api endpoint', async (done) => {
+    const res = await request
+      .get('/products/1')
+      .set('Authorization', 'Bearer ' + token);
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: 1,
+      name: 'iphone',
+      price: '3000',
+      category: 'phone'
+    });
+    done();
+  });
+  it('gets product by category api endpoint', async (done) => {
+    const res = await request
+      .get('/products/cat/phone')
+      .set('Authorization', 'Bearer ' + token);
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual([
+      {
+        id: 1,
+        name: 'iphone',
+        price: '3000',
+        category: 'phone'
+      }
+    ]);
+    done();
+  });
+  it('create product api endpoint', async (done) => {
+    const res = await request
+      .post('/products')
+      .set('Authorization', 'Bearer ' + token);
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: 1,
+      name: 'iphone',
+      price: '3000',
+      category: 'phone'
+    });
+    done();
+  });
+  it('delets a product api endpoint', async (done) => {
+    const res = await request
+      .delete('/products/1')
+      .set('Authorization', 'Bearer ' + token);
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: 1,
+      name: 'iphone',
+      price: '3000',
+      category: 'phone'
+    });
+    done();
+  });
+});
